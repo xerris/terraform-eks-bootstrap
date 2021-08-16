69b416ed7f4d6cb1a8beb7f627d7ba22059a426b
Andres Torres <andres.torresduran@gmail.com>
2021-08-16 12:52:38 -0600

    creating VPC from scratch

--- terraform_exec.sh
+++ terraform_exec.sh
@@ -8,13 +8,13 @@ echo "## Starting Terraform script ##"
 echo "###############################"
 
 ENV="${ENV:-dev}"
-AWS_REGION="${AWS_REGION:-us-east-1}"
+AWS_REGION="${AWS_REGION:-ca-central-1}"
 echo "Configuring AWS Profiles"
 aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID --profile kurbee-user
 aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY --profile kurbee-user
 aws configure set role_arn "arn:aws:iam::${ACCOUNT_ID}:role/deployment-role" --profile deployment-role
 aws configure set source_profile kurbee-user --profile deployment-role
-aws configure set role_session_name aldo-test-session --profile deployment-role
+aws configure set role_session_name test-session --profile deployment-role
 export AWS_PROFILE=deployment-role
 
 APPLY=${1:-0} #If set terraform will force apply changes
@@ -23,12 +23,14 @@ build_number="${BITBUCKET_BUILD_NUMBER:=local}"
 #export TF_LOG=TRACE
 export TF_VAR_commit_hash="${commit_hash}"
 export TF_VAR_build_number="${build_number}"
-
+echo $ACCOUNT_ID
+echo $ENV
+echo $AWS_REGION
 terraform init \
 -backend-config="bucket=project-eks-terraform-state-${ENV}" \
 -backend-config="key=${ENV}/project-eks-bootstrap.tfstate" \
 -backend-config="dynamodb_table=${ENV}-project-eks-terraform-state-lock-dynamo" \
--backend-config="region=${AWS_REGION}"
+-backend-config="region=${AWS_REGION}" \
 -backend-config="role_arn=arn:aws:iam::${ACCOUNT_ID}:role/deployment-role" \
 -backend-config="session_name=${ENV}-session"
 
