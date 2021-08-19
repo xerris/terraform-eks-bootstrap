ec9af2328ec5a9000d73698aeaf6763e294f2d33
aetorres <andres.torresduran@gmail.com>
2021-08-19 12:20:00 -0600

    changing machine executor by Docker containers

--- terraform_exec.sh
+++ terraform_exec.sh
@@ -12,14 +12,14 @@ AWS_REGION="${AWS_REGION:-ca-central-1}"
 echo "Configuring AWS Profiles"
 export AWS_PROFILE=default
 
-temp_role=$(aws sts assume-role --role-arn "arn:aws:iam::${ACCOUNT_ID}:role/deployment-role" --role-session-name "RoleSession1" --profile default)
-aws configure set aws_access_key_id $(echo $temp_role | jq .Credentials.AccessKeyId | xargs) --profile deployment-profile
-aws configure set aws_secret_access_key $(echo $temp_role | jq .Credentials.SecretAccessKey | xargs) --profile deployment-profile
-aws configure set aws_session_token $(echo $temp_role | jq .Credentials.SessionToken | xargs) --profile deployment-profile
+#temp_role=$(aws sts assume-role --role-arn "arn:aws:iam::${ACCOUNT_ID}:role/deployment-role" --role-session-name "RoleSession1" --profile default)
+#aws configure set aws_access_key_id $(echo $temp_role | jq .Credentials.AccessKeyId | xargs) --profile deployment-profile
+#aws configure set aws_secret_access_key $(echo $temp_role | jq .Credentials.SecretAccessKey | xargs) --profile deployment-profile
+#aws configure set aws_session_token $(echo $temp_role | jq .Credentials.SessionToken | xargs) --profile deployment-profile
 
-#aws configure set role_arn "arn:aws:iam::${ACCOUNT_ID}:role/deployment-role" --profile deployment-profile
-#aws configure set source_profile default --profile deployment-profile
-#aws configure set role_session_name test-session --profile deployment-profile
+aws configure set role_arn "arn:aws:iam::${ACCOUNT_ID}:role/deployment-role" --profile deployment-profile
+aws configure set source_profile default --profile deployment-profile
+aws configure set role_session_name test-session --profile deployment-profile
 export AWS_PROFILE=deployment-profile
 
 APPLY=${1:-0} #If set terraform will force apply changes
@@ -34,7 +34,6 @@ terraform init \
 -backend-config="bucket=project-eks-terraform-state-${ENV}" \
 -backend-config="key=${ENV}/project-eks-bootstrap.tfstate" \
 -backend-config="dynamodb_table=${ENV}-project-eks-terraform-state-lock-dynamo" \
--backend-config="profile=deployment-profile" \
 -backend-config="region=${AWS_REGION}"
 
 
