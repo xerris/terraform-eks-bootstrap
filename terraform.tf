d84e69b5aab1401ada5b2869b957c5de4768e24a
aetorres <andres.torresduran@gmail.com>
2021-12-17 09:26:57 -0600

    updating Readme

--- terraform.tf
+++ terraform.tf
@@ -10,8 +10,5 @@ terraform {
 
 provider "aws" {
   region  = var.region
-  #assume_role {
-  #  role_arn     = "arn:aws:iam::${var.account_id}:role/aldo-jenkins"
-  #  session_name = "${var.env}-omni-dataapps"
-  #}
+
 }
