e4c2261c39efb6b8bb977ec625a7e88d401446a8
aetorres <andres.torresduran@gmail.com>
2022-03-08 13:58:37 -0600

    adding flux2 to initial config

--- cicd/main.tf
+++ cicd/main.tf
@@ -1,5 +1,6 @@
 module "flux2"{
-    source = "./cicd/flux2"
+    count = var.flux2 ? 1 : 0
+    source = "./flux2"
     target_path = "${var.target_path}/${var.env}"
     repository_name = "terraform-eks-apps-bootstrap"
     repo_url = "https://github.com/${var.github_owner}/${var.repository_name}"
