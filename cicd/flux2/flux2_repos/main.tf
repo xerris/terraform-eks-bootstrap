cadb1ff9c9f060bfa11175d8984587b870f9e348
iamginu <hotspotwifi5@gmail.com>
2022-12-06 08:17:13 -0500

    changes - 18

--- cicd/flux2/flux2_repos/main.tf
+++ cicd/flux2/flux2_repos/main.tf
@@ -88,7 +88,7 @@ resource "kubectl_manifest" "sync" {
 resource "kubernetes_secret" "main" {
   count      = local.flux2["enabled"] ? 1 : 0
   metadata {
-    name      = data.flux_sync.main[0].secret
+    name      = "ap-${data.flux_sync.main[0].secret}"
     namespace = data.flux_sync.main[0].namespace
   }
 
