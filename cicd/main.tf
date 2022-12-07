ad7e8d8f6f607ee389a31cb446e2b321bb617f3a
iamginu <hotspotwifi5@gmail.com>
2022-12-07 08:48:09 -0500

    changes - 25

--- cicd/main.tf
+++ cicd/main.tf
@@ -19,29 +19,29 @@ module "flux2_crd"{
 #    ready = module.flux2_crd.manifest_ready
 #}
 ##
-# module "flux_repo_2048_stage"{
-#     source = "./flux2/flux2_repos"
-#     repository_name = "2048-k8-app"
-#     repo_url = "https://github.com/${var.github_owner}/${var.repository_name}"
-#     branch = "xdp-ginu-dev"
-#     target_path = "${var.target_path}/stage"
-#     flux_token  = var.flux_token
-#     repo_provider = var.repo_provider
-#     region =  var.region
-#     github_user = var.github_user
-#     ready = module.flux2_crd.manifest_ready
-# }
+module "flux_repo_2048_stage"{
+    source = "./flux2/flux2_repos"
+    repository_name = "2048-k8-app"
+    repo_url = "https://github.com/${var.github_owner}/${var.repository_name}"
+    branch = "xdp-ginu-dev"
+    target_path = "${var.target_path}/stage"
+    flux_token  = var.flux_token
+    repo_provider = var.repo_provider
+    region =  var.region
+    github_user = var.github_user
+    ready = module.flux2_crd.manifest_ready
+}
 
 
-# module "flux_repo_addons"{
-#     source = "./flux2/flux2_repos"
-#     repository_name = "kubernetes-addons-bootstrap"
-#     repo_url = "https://github.com/${var.github_owner}/kubernetes-addons-bootstrap"
-#     branch = var.branch
-#     target_path = "${var.target_path}/${var.env}"
-#     flux_token  = var.flux_token
-#     repo_provider = var.repo_provider
-#     region =  var.region
-#     github_user = var.github_user
-#     ready = module.flux2_crd.manifest_ready
-# }
+module "flux_repo_addons"{
+    source = "./flux2/flux2_repos"
+    repository_name = "kubernetes-addons-bootstrap"
+    repo_url = "https://github.com/${var.github_owner}/kubernetes-addons-bootstrap"
+    branch = var.branch
+    target_path = "${var.target_path}/${var.env}"
+    flux_token  = var.flux_token
+    repo_provider = var.repo_provider
+    region =  var.region
+    github_user = var.github_user
+    ready = module.flux2_crd.manifest_ready
+}
